c59f7b75df80bef07954c8e8a708b32c3820b3ac
traiquanhp <tunmhe171130@fpt.edu.vn>
2025-12-01 16:51:46 +0700

    fixManageResident

--- ApartaAPI/Services/Interfaces/IApartmentMemberService.cs
+++ ApartaAPI/Services/Interfaces/IApartmentMemberService.cs
@@ -6,6 +6,8 @@ namespace ApartaAPI.Services.Interfaces
     public interface IApartmentMemberService
     {
         Task<ApiResponse<IEnumerable<ApartmentMemberDto>>> GetAllAsync(ApartmentMemberQueryParameters query);
+        Task<ApiResponse<IEnumerable<ApartmentMemberDto>>> GetByUserBuildingsAsync(string userId, ApartmentMemberQueryParameters query);
+
         Task<ApartmentMemberDto?> GetByIdAsync(string id);
         Task<ApartmentMemberDto> CreateAsync(ApartmentMemberCreateDto dto,IFormFile? faceImageFile = null,CancellationToken cancellationToken = default);
         Task<bool> UpdateAsync(string id, ApartmentMemberUpdateDto dto);
